1ee2969aa388cab78fa6e391c4c63fcc2dd932c0
Angel Perez <aperez@informa.es>
2020-06-01 19:33:11 +0200

    first commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-header
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Music
+          .nav-right.nav-menu
+            router-link.nav-item(:to="{ name: 'search' }") Buscar
+            router-link.nav-item(to="about") Nosotros
+    .hero-body
+      .container.has-text-centered
+        h1.tittle Music!
+        h2.subtittle Canciones vuenardas
+        pm-player
+</template>
+<script>
+  import PmPlayer from '@/components/Player.vue'
+
+  export default {
+    components: { PmPlayer }
+  }
+</script>
---
+++ src/services/track.js
@@ -0,0 +1,15 @@
+import musicService from './my-music'
+
+const trackService = {}
+
+trackService.search = function (q) {
+  const type = 'track'
+
+  return musicService.get('/search', { params: { q, type } }).then(res => res.data)
+}
+
+trackService.getById = function (id) {
+  return musicService.get(`/tracks/${id}`).then(res => res.data)
+}
+
+export default trackService
